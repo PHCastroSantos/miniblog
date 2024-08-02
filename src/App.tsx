f14b09f4ff3a7f0b926b479fcc6398b10a1a7edc
Pedro Santos <phcastroestudoss@gmail.com>
2024-08-02 15:36:07 -0300

    Adicionando mais funções, autenticação e estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
+
+// Hooks
+import { useState, useEffect } from 'react'
+import { useAuthentication } from './hooks/useAuthentication'
+
+// Context
+import { AuthProvider } from './context/AuthContext'
 
 // Pages
 import Home from './pages/Home/Home'
@@ -13,22 +21,42 @@ import Footer from './components/Footer'
 
 function App() {
 
+  const [user, setUser] = useState(undefined)
+  const [auth] = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+
+  }, [auth])
+
+  if(loadingUser) {
+    return <p>Carregando...</p>
+  }
+
+
 
   return (
     <>
       <div>
+  <AuthProvider>
         <BrowserRouter>
-        <div className="container">
-          <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
-        </div>
-        <Footer/>
+          <div className="container">
+            <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+          </div>
+          <Footer/>
         </BrowserRouter>
+    </AuthProvider>
       </div>
 
     </>
